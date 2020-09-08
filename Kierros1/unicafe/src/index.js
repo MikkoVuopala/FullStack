3a9e8d579f37b66a574bc35ce61ddc7dc9651a5a
Mikko <mikko.vuopala98@gmail.com>
2020-09-08 13:08:22 +0300

    1.7 tehtävä vaiheessa

--- Kierros1/unicafe/src/index.js
+++ Kierros1/unicafe/src/index.js
@@ -6,16 +6,35 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
+
+  const goodClick = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const badClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick = {() => setGood(good + 1)}>Good :)</button>
-      <button onClick = {() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick = {() => setBad(bad + 1)}>Bad :(</button>
+      <button onClick = {goodClick}>Good :)</button>
+      <button onClick = {neutralClick}>Neutral</button>
+      <button onClick = {badClick}>Bad :(</button>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {}</p>
+      <p>Positive: {}</p>
     </div>
   )
 }
