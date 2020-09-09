ab3d7306df3100999990c5e565ebbeafad27b197
Mikko <mikko.vuopala98@gmail.com>
2020-09-09 11:27:30 +0300

    1.11 tehty / unicafe valmis

--- Kierros1/unicafe/src/index.js
+++ Kierros1/unicafe/src/index.js
@@ -7,16 +7,18 @@ let posCounter = 0
 const StatisticLine = (props) => {
   if (props.text === 'Positive:') {
     return (
-      <div>
-        <p>{props.text} {props.value} %</p>
-      </div>
+      <tr>
+        <th>{props.text}</th>
+        <td>{props.value} %</td>
+      </tr>
     )
   }
 
   return (
-    <div>
-      <p>{props.text} {props.value}</p>
-    </div>
+    <tr>
+      <th>{props.text}</th>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -31,12 +33,16 @@ const Statistics = (props) => {
 
   return (
     <div>
-      <StatisticLine text = 'Good:' value = {props.good}/>
-      <StatisticLine text='Neutral:' value={props.neutral}/>
-      <StatisticLine text='Bad:' value={props.bad}/>
-      <StatisticLine text='All:' value={props.all}/>
-      <StatisticLine text='Average:' value={props.average}/>
-      <StatisticLine text='Positive:' value={props.pos} />
+      <table>
+        <tbody>
+        <StatisticLine text = 'Good:' value = {props.good}/>
+        <StatisticLine text='Neutral:' value={props.neutral}/>
+        <StatisticLine text='Bad:' value={props.bad}/>
+        <StatisticLine text='All:' value={props.all}/>
+        <StatisticLine text='Average:' value={props.average}/>
+        <StatisticLine text='Positive:' value={props.pos} />
+        </tbody>
+      </table>
     </div>
   )
 }
