af5ce146bbea2161c748a8bec57a49ae9203cb40
Mikko <mikko.vuopala98@gmail.com>
2020-09-09 11:14:26 +0300

    1.10 tehtynä

--- Kierros1/unicafe/src/index.js
+++ Kierros1/unicafe/src/index.js
@@ -4,6 +4,22 @@ import ReactDOM from 'react-dom'
 let counter = 0
 let posCounter = 0
 
+const StatisticLine = (props) => {
+  if (props.text === 'Positive:') {
+    return (
+      <div>
+        <p>{props.text} {props.value} %</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <p>{props.text} {props.value}</p>
+    </div>
+  )
+}
+
 const Statistics = (props) => {
   if (props.all === 0) {
     return (
@@ -15,15 +31,19 @@ const Statistics = (props) => {
 
   return (
     <div>
-    <p>Good: {props.good}</p>
-    <p>Neutral: {props.neutral}</p>
-    <p>Bad: {props.bad}</p>
-    <p>All: {props.all}</p>
-    <p>Average: {props.average}</p>
-    <p>Positive: {props.pos + ' %'}</p>
+      <StatisticLine text = 'Good:' value = {props.good}/>
+      <StatisticLine text='Neutral:' value={props.neutral}/>
+      <StatisticLine text='Bad:' value={props.bad}/>
+      <StatisticLine text='All:' value={props.all}/>
+      <StatisticLine text='Average:' value={props.average}/>
+      <StatisticLine text='Positive:' value={props.pos} />
     </div>
   )
 }
+
+const Button = ({ onClick, text }) => (
+  <button onClick = {onClick}>{text}</button>
+)
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -56,9 +76,9 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick = {goodClick}>Good :)</button>
-      <button onClick = {neutralClick}>Neutral</button>
-      <button onClick = {badClick}>Bad :(</button>
+      <Button onClick = {goodClick} text = 'Good :)'/>
+      <Button onClick = {neutralClick} text = 'Neutral' />
+      <Button onClick = {badClick} text = 'Bad'/>
       <br></br>
       <h2>Statistics</h2>
       <Statistics
