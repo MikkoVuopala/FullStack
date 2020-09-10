f58f916f77b038e74861b80dfa575459de1ef5ee
Mikko <mikko.vuopala98@gmail.com>
2020-09-10 12:22:28 +0300

    2.1 tehty

--- Kierros2/kurssitiedot2/src/index.js
+++ Kierros2/kurssitiedot2/src/index.js
@@ -7,27 +7,32 @@ const Header = ({ course }) => {
   )
 }
 
-const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+const Part = ({parts}) => {
   return (
-    <p>Number of exercises {sum}</p>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
+    <div>
+    {parts.map(part =>
+      <p key = {part.id}>
+      {part.name} {part.exercises}
     </p>
+    )}
+    </div>
   )
 }
 
 const Content = ({ course }) => {
   return (
     <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      <Part parts={course.parts} />
+      
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content course = {course}/>
     </div>
   )
 }
@@ -35,27 +40,34 @@ const Content = ({ course }) => {
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Testing',
+        exercises: 15,
+        id: 4
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Course course={course} />
     </div>
   )
 }
