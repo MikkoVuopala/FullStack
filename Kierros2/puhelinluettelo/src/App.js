9c1c2314e841b7df426e0f4557d33f857bc33a93
Mikko <mikko.vuopala98@gmail.com>
2020-09-17 11:46:36 +0300

    2.9 tehty

--- Kierros2/puhelinluettelo/src/App.js
+++ Kierros2/puhelinluettelo/src/App.js
@@ -7,6 +7,8 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [filtPersons, setfiltPersons] = useState([])
 
   const addName = (event) => {
     event.preventDefault()
@@ -37,9 +39,23 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+    setfiltPersons(persons.filter(p => p.name.toLowerCase().includes(event.target.value.toLowerCase())))
+  }
+
+  
+  if (filter === '') {
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>filtering with: <input
+                              value = {filter}
+                              onChange = {handleFilterChange}/>
+      </div>
       <form onSubmit = {addName}>
         <div>
           name: <input 
@@ -62,6 +78,36 @@ const App = () => {
     
   )
 
+} else {
+    return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <div>filtering with: <input
+        value={filter}
+        onChange={handleFilterChange} />
+      </div>
+      <form onSubmit={addName}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={handleNameChange} />
+          <br></br>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {filtPersons.map(p =>
+        <p key={p.name}>{p.name} {p.phoneNumber}</p>
+      )}
+    </div>
+    )
+}
 }
 
 export default App
