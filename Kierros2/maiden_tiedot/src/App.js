3d495867f5b8da5d71f321a9fb4cbf4c568ae5a7
Mikko <mikko.vuopala98@gmail.com>
2020-09-18 12:54:34 +0300

    2.12 tehty

--- Kierros2/maiden_tiedot/src/App.js
+++ Kierros2/maiden_tiedot/src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const Filter = ({value, onChange}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Find countries: <input value={value} onChange={onChange} />
     </div>
-  );
+  )
+}
+
+const Show = (props) => {
+  if (props.countries.length > 10) {
+    return (
+      <div>
+        <p>Too many matches, specify another filter</p>
+      </div>
+    )
+  } else if (props.countries.length > 1 && props.countries.length <= 10){
+    return (
+      <div>
+        {
+          props.countries.map(x =>
+            <p key = {x.name}>{x.name}</p>
+          )
+        }
+      </div>
+    )
+  } else if (props.countries.length === 1) {
+    return (
+      <div>
+        <h3>{props.countries[0].name}</h3>
+        <p>Capital: {props.countries[0].capital}</p>
+        <p>Population: {props.countries[0].population}</p>
+        <h4>Languages:</h4>
+        {
+          props.countries[0].languages.map(x =>
+            <p key = {x.name}>{x.name}</p>
+          )
+        }
+        <img src = {props.countries[0].flag} alt = "Flag"></img>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <p>No country matches your search.</p>
+      </div>
+    )
+  }
+}
+
+const App = () => {
+    
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  const [fCountries, setFC] = useState([])
+  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log(response.data)
+        setCountries(response.data)
+        setFC(response.data)
+      })
+  }, [])
+
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+    setFC(countries.filter(x => x.name.toLowerCase().includes(event.target.value.toLowerCase())))
+    console.log(fCountries.length)
+  } 
+  
+  return (
+    <div>
+      <Filter value = {filter} onChange = {handleSearch}/>
+      <Show countries = {fCountries}/>
+    </div>
+  )
 }
 
 export default App;
--- Kierros2/maiden_tiedot/src/index.js
+++ Kierros2/maiden_tiedot/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const promise = axios.get('https://restcountries.eu/rest/v2/all')
+console.log(promise)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
